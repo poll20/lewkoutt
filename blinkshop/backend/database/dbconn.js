b3d28f3f2523cca3ec3c3dc14626d4a80a8a9a02
abhay sharma <sharmaabhay1549@gmail.com>
2025-03-26 16:49:53 +0530

    databse connection changes

--- blinkshop/backend/database/dbconn.js
+++ blinkshop/backend/database/dbconn.js
@@ -1,5 +1,7 @@
 let mongoose=require("mongoose")
 console.log("MongoDB URI:", process.env.MONGO_URI);
+mongoose.set('bufferCommands', false); // To avoid command buffering
+mongoose.set('serverSelectionTimeoutMS', 30000); // Increase timeout to 30s
  mongoose.connect(process.env.MONGO_URI)
  .then(()=>{
     console.log("connect successfully")
