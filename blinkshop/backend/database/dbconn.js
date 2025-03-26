3ef7b26d906c4c6a78cf48ebd2849b395240a8c0
abhay sharma <sharmaabhay1549@gmail.com>
2025-03-26 17:11:53 +0530

    databse connection changes

--- blinkshop/backend/database/dbconn.js
+++ blinkshop/backend/database/dbconn.js
@@ -1,5 +1,10 @@
 let mongoose=require("mongoose")
-console.log("MongoDB URI:", process.env.MONGO_URI);
+console.log("MongoDB URI:", process.env.MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverSelectionTimeoutMS: 50000, // Server selection timeout बढ़ाया
+   socketTimeoutMS: 45000, // Query execution का timeout बढ़ाया
+ });
 mongoose.set('bufferCommands', false); // To avoid command buffering
 mongoose.set('serverSelectionTimeoutMS', 30000); // Increase timeout to 30s
  mongoose.connect(process.env.MONGO_URI)
