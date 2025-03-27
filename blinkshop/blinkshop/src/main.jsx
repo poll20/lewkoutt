de777bb29336e4b479bfddd75f72fb4d91ad2883
abhay sharma <sharmaabhay1549@gmail.com>
2025-03-27 12:36:26 +0530

    databse connection changes

--- blinkshop/blinkshop/src/main.jsx
+++ blinkshop/blinkshop/src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
   
    <Auth0Provider domain={import.meta.env.VITE_AUTH0_DOMAIN} clientId={import.meta.env.VITE_AUTH0_CLIENT_ID} authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL
     }}>
     <App/>
     </Auth0Provider>
