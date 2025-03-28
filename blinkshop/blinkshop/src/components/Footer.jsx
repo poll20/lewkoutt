986683ff121702eebf680acd5820b1fde389302d
abhay sharma <sharmaabhay1549@gmail.com>
2025-03-28 10:12:02 +0530

    footer fixs

--- blinkshop/blinkshop/src/components/Footer.jsx
+++ blinkshop/blinkshop/src/components/Footer.jsx
@@ -24,7 +24,7 @@ useEffect(()=>{
 },[])
 
 console.log("popo",cat)
-let data=cat.map((e)=>(e.productdetails,map((e)=>(e.tag))))
+let data=cat.map((e)=>(e.productdetails.map((e)=>(e.tag))))
 console.log("cucu",data)
 
 let distinctcat=[... new Set(data)]
