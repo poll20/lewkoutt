db45144f91faf1ea3982ffd95655e4d7c925a3f8
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-07 19:12:20 +0530

    kafi file edit kiiiii

--- blinkshop/blinkshop/src/components/Navbar.jsx
+++ blinkshop/blinkshop/src/components/Navbar.jsx
@@ -148,7 +148,7 @@ let closeslidecategorynav=()=>{
         className="absolute -top-1 -right-1 bg-red-600 text-white text-xs w-5 h-5 rounded-full
                    flex items-center justify-center font-bold"
       >
-        {addtocartdatas.length}
+       5
       </span>
     )}
   </div>
