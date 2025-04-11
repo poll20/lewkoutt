ddd47ce3a62401355c4c17ffc46829ca2647ae72
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-11 11:17:50 +0530

    kafi file editttttttttttttttttttttttttttttttttttttttttttttttttttt

--- blinkshop/blinkshop/src/components/GlobalLoader.jsx
+++ blinkshop/blinkshop/src/components/GlobalLoader.jsx
@@ -8,24 +8,24 @@ const GlobalLoader = () => {
   if (!isLoading) return null;
   console.log("GlobalLoader mounted. isLoading:", isLoading);
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        zIndex: 9999,
-        width: "100vw",
-        height: "100vh",
-        background: "rgba(255, 255, 255, 0.7)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    // <div
+    //   style={{
+    //     position: "fixed",
+    //     top: 0,
+    //     left: 0,
+    //     zIndex: 9999,
+    //     width: "100vw",
+    //     height: "100vh",
+    //     background: "rgba(255, 255, 255, 0.7)",
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
 <div class="spinner-border" role="status">
   <span class="visually-hidden">Loading...</span>
 </div>
-    </div>
+    // </div>
   );
 };
 
