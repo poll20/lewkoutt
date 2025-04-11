67e99e51aee23066c8dae81e7f5350c3412218dd
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-11 10:57:36 +0530

    kafi file edittttttttttttttttttttttttttttttttttttttttttttttttt

--- blinkshop/blinkshop/src/components/LoadingContext.jsx
+++ blinkshop/blinkshop/src/components/LoadingContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useState } from "react";
 const LoadingContext = createContext();
 
 export const LoadingProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
