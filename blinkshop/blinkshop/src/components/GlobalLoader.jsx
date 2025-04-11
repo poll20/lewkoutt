fcda0a86839c7a4f525d92bbcbabbf7b46231998
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-11 11:35:39 +0530

    kafi file edittttttttttttttttttttttttttttttttttttttttttttttttttttttt

--- blinkshop/blinkshop/src/components/GlobalLoader.jsx
+++ blinkshop/blinkshop/src/components/GlobalLoader.jsx
@@ -8,24 +8,24 @@ const GlobalLoader = () => {
   if (!isLoading) return null;
   console.log("GlobalLoader mounted. isLoading:", isLoading);
   return (
-    // <div
-    //   style={{
-    //     position: "fixed",
-    //     top: 0,
-    //     left: 0,
-    //     zIndex: 9999,
-    //     width: "100vw",
-    //     height: "100vh",
-    //     background: "rgba(255, 255, 255, 0.7)",
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //   }}
-    // >
-<div class="spinner-border" role="status">
-  <span class="visually-hidden">Loading...</span>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 9999,
+        width: "100vw",
+        height: "100vh",
+        background: "rgba(255, 255, 255, 0.7)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+<div className="h-16 w-16 bg-black text-white text-center flex items-center justify-center rounded-full">
+  Loading...
 </div>
-    // </div>
+    </div>
   );
 };
 
