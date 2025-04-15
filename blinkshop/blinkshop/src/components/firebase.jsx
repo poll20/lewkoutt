dade52f9655cdaedb1f65fc73f70f9880a420d4f
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-15 18:01:49 +0530

    file fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- blinkshop/blinkshop/src/components/firebase.jsx
+++ blinkshop/blinkshop/src/components/firebase.jsx
@@ -12,8 +12,10 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_APP_ID,
     measurementId:import.meta.env.VITE_MEASUREMENT_ID
 };
+console.log("Firebase config:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
+console.log("App initialized:", app);
 const auth = getAuth(app);
 
 export { auth, RecaptchaVerifier, signInWithPhoneNumber,PhoneAuthProvider,signInWithCredential,signOut,onAuthStateChanged};
