2bc180607c92bd655bf4c8a3614d43167146c043
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-16 01:22:15 +0530

    file fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- blinkshop/blinkshop/src/components/firebase.jsx
+++ blinkshop/blinkshop/src/components/firebase.jsx
@@ -4,13 +4,13 @@ console.log("API Key:", import.meta.env.VITE_API_KEY);
 console.log("API Key:", import.meta.env.VITE_AUTH_DOMAIN);
 console.log("API Key:", import.meta.env.VITE_PROJECT_ID);
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId:import.meta.env.VITE_MEASUREMENT_ID
+    apiKey:"AIzaSyCygJkXmV9nEHzShM6xrW5qX5qYq9c2nLg",
+    authDomain:"lewkout-bcf84.firebaseapp.com",
+    projectId:"lewkout-bcf84",
+    storageBucket:"lewkout-bcf84.appspot.com",
+    messagingSenderId:"315965564771",
+    appId:"1:315965564771:web:dfd6439dd1330ce8925556",
+    measurementId:"G-EYX24MYHNB"
 };
 console.log("Firebase config:", firebaseConfig);
 
