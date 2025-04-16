3cd8b9c338a2bb64c0110cffd90aa017ec2e4033
abhay sharma <sharmaabhay1549@gmail.com>
2025-04-16 08:59:11 +0530

    file fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxz

--- blinkshop/blinkshop/src/components/firebase.jsx
+++ blinkshop/blinkshop/src/components/firebase.jsx
@@ -3,15 +3,24 @@ import { getAuth, RecaptchaVerifier, signInWithPhoneNumber ,PhoneAuthProvider,si
 console.log("API Key:", import.meta.env.VITE_API_KEY);
 console.log("API Key:", import.meta.env.VITE_AUTH_DOMAIN);
 console.log("API Key:", import.meta.env.VITE_PROJECT_ID);
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_API_KEY,
+//     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//     projectId: import.meta.env.VITE_PROJECT_ID,
+//     storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//     appId: import.meta.env.VITE_APP_ID,
+//     measurementId:import.meta.env.VITE_MEASUREMENT_ID
+// };
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
-    measurementId:import.meta.env.VITE_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyCpLrvP0mFks8Fg0BvTRqa4yPb3Jco3--w",
+    authDomain: "lewkout-bcf84.firebaseapp.com",
+    projectId: "lewkout-bcf84",
+    storageBucket: "lewkout-bcf84.firebasestorage.app",
+    messagingSenderId: "315965564771",
+    appId: "1:315965564771:web:dfd6439dd1330ce8925556",
+    measurementId: "G-EYX24MYHNB"
+  };
 console.log("Firebase config:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
