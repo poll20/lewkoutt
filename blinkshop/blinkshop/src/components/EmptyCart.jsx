ecd769ac35d57e1f52bee89baf6ddd857c4c0ef5
abhay sharma <sharmaabhay1549@gmail.com>
2025-05-20 19:46:25 +0530

    filesss editusaa111111111

--- blinkshop/blinkshop/src/components/EmptyCart.jsx
+++ blinkshop/blinkshop/src/components/EmptyCart.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./EmptyCart.css";
 import { FaShoppingCart,FaHeart  } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import animation from "./image/emptywish.mp4"
 const EmptyCart = (props) => {
     console.log("zzz"+props.endpoint)
     let navigate=useNavigate()
@@ -11,7 +12,9 @@ const EmptyCart = (props) => {
 
   return (
     <div className="empty-cart-container">
-     {props.endpoint=="cart"?( <FaShoppingCart className="cart-icon" />):(<FaHeart className="cart-icon"/>)}
+     {props.endpoint=="cart"?( <FaShoppingCart className="cart-icon" />):(<video autoPlay loop muted playsInline style={{width:"200px",height:"200px"}}>
+  <source src={animation} type="video/mp4"  />
+</video>)}
       <h2>{props.endpoint.slice(0,8)} is Empty</h2>
       <p>Look like you hav'nt added anythong yet. Start exploring and save your favorites</p>
       <button onClick={handleShopNow} className="shop-now-button" >
