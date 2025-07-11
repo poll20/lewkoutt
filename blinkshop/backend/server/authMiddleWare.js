12e3dcb327ba6a5f3d781878dfd6652ed4ebbc7e
abhay sharma <sharmaabhay1549@gmail.com>
2025-07-12 00:19:26 +0530

    key edited

--- blinkshop/backend/server/authMiddleWare.js
+++ blinkshop/backend/server/authMiddleWare.js
@@ -4,11 +4,18 @@ const admin = require("firebase-admin");
 // Firebase Admin SDK initialize
 
 
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     projectId: process.env.FB_PROJECT_ID,
+//     clientEmail: process.env.FB_CLIENT_EMAIL,
+//     privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//   }),
+// });
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FB_PROJECT_ID,
     clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
 });
 
