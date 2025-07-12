39c5c7e6489a01e181c875fc3aabe03d80f87c5c
abhay sharma <sharmaabhay1549@gmail.com>
2025-07-12 11:14:03 +0530

    key editeddd

--- blinkshop/backend/server/authMiddleWare.js
+++ blinkshop/backend/server/authMiddleWare.js
@@ -16,7 +16,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FB_PROJECT_ID,
     clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY,
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
 });
 
