816c1e882dbc7829096bb27263f25f7cc159d4f0
abhay sharma <sharmaabhay1549@gmail.com>
2025-07-12 11:11:45 +0530

    key editedd

--- blinkshop/backend/server/authMiddleWare.js
+++ blinkshop/backend/server/authMiddleWare.js
@@ -11,11 +11,12 @@ const admin = require("firebase-admin");
 //     privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
 //   }),
 // });
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FB_PROJECT_ID,
     clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.FB_PRIVATE_KEY,
   }),
 });
 
