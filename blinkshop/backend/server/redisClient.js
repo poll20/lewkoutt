d2323c6b2586ed994d9de952787e1840b84b06b8
abhay sharma <sharmaabhay1549@gmail.com>
2025-09-14 14:52:27 +0530

    redis124

---
+++ blinkshop/backend/server/redisClient.js
@@ -0,0 +1,14 @@
+// server/redisClient.js
+const redis = require("redis");
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+(async () => {
+  await client.connect();
+})();
+
+module.exports = client;
