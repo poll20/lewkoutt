c64fa2a1533e9f6f055f64b5e8b7755b19ef33c3
abhay sharma <sharmaabhay1549@gmail.com>
2025-09-20 16:42:10 +0530

    gs444

--- blinkshop/blinkshop/src/analytics/usePageTracking.jsx
+++ blinkshop/blinkshop/src/analytics/usePageTracking.jsx
@@ -1,8 +1,7 @@
 // src/analytics/usePageTracking.js
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { trackPageView } from "./ga4";
-
+import { trackPageView } from "./ga4"; // correct spelling
 export default function usePageTracking() {
   const location = useLocation();
 
